62ff32ed8ad55dcdeae2f324c704d47026469d98
etienne428 <et.mettaz@stud.unibas.ch>
2021-01-27 21:10:11 +0100

    Working project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,10 +7,13 @@ public class Main {
     public void addTables() throws SQLException {
         String insertTable = "LOAD DATA INFILE ?" +
                 "INTO TABLE ?" +
-                "FIELDS TERMINATED BY ','" +
-                "LINES TERMINATED BY '\n'" +
-                "IGNORE 1 LINES ;";
-
+                "FIELDS TERMINATED BY \',\'" +
+                "LINES TERMINATED BY \'\\n\'" +
+                "IGNORE 1 LINES";
+//        String esquel = " LOAD DATA LOCAL INFILE '?" +
+//                "' INTO TABLE ?"+
+//                " FIELDS TERMINATED BY \',\' ENCLOSED BY \'\"'" +
+//                " LINES TERMINATED BY \'\\n\'";
         PreparedStatement statement = con.prepareStatement(insertTable);
         // City name, population and density (inhabitant / m^2)
         // Source : https://www.ukpopulation.org/london-population/
@@ -21,13 +24,12 @@ public class Main {
         // https://www.statistik.bs.ch/haeufig-gefragt/basel-kompakt.html
 //        statement.executeUpdate("INSERT INTO Location " + "VALUES ('Basel', 200407, 5574)");
 
-
-        String[] CSVFile = {"Detectors.csv", "LinksCleaned.csv", "ETHTrafficCleaned.csv", "MVIcleaned.csv", "AQ_cleaned.csv"};//, "Weather_cleaned.csv"};
+        String[] CSVFile = {"Detectors.csv'", "LinksCleaned.csv'", "ETHTrafficCleaned.csv'", "MVIcleaned.csv'", "AQ_cleaned.csv'"};//, "Weather_cleaned.csv'"};
         String[] tableName = {"Detectors", "Links", "UTD", "MVI", "AirPollution"};//, "Weather"};
 
         for (int i = 0; i < 1/*CSVFile.length*/; i++) {
-            statement.setString(1, "Resources\\" + CSVFile[i]);
-            statement.setString(2, tableName[i]);
+            statement.setString(1, "'src/main/resources/" + CSVFile[4]);
+            statement.setString(2, tableName[4]);
             statement.addBatch();
         }
         statement.executeBatch();
